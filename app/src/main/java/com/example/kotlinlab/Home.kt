ad85bbe2184400a5190fe66f4b4513dff827f38a
CannierHesh <ysrafles@gmail.com>
2020-05-29 10:01:24 +0530

    kotlab

---
+++ app/src/main/java/com/example/kotlinlab/Home.kt
@@ -0,0 +1,13 @@
+package com.example.loginform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlinlab.R
+
+class Home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
